e3512f7955468fc40bb3e0ca6c4bde5163da617d
phil-griffith <philgriffith@outlook.com>
2021-05-30 08:10:18 +0000

    Add product detail template & functionality

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -14,3 +14,15 @@ def shop(request):
     }
 
     return render(request, 'shop/shop.html', context)
+
+
+def product_detail(request, product_id):
+    """ View to display product detail """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/product_detail.html', context)
