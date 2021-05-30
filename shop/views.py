8bb3571f6fd4f28e30ebc998cb40814cca58a026
phil-griffith <philgriffith@outlook.com>
2021-05-30 10:41:13 +0000

    Add search functionality

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -8,9 +10,22 @@ def shop(request):
     """ View to return shop page """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                print("No search term entered!!")
+                return redirect(reverse('shop'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'shop/shop.html', context)
