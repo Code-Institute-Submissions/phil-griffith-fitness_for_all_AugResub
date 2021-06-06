80930206df5462af24c84cf73b3544f1372846f6
phil-griffith <philgriffith@outlook.com>
2021-06-06 17:49:09 +0000

    Add add, edit & delete functionality to workouts

--- workouts/views.py
+++ workouts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Workouts, Category
+from .forms import WorkoutsForm
 
 
 
@@ -63,6 +64,74 @@ def workout(request, workout_id):
     }
 
     return render(request, 'workouts/workout.html', context)
+
+
+@login_required
+def add_workout(request):
+    """ Add a workout  """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = WorkoutsForm(request.POST, request.FILES)
+        if form.is_valid():
+            workout = form.save()
+            messages.success(request, 'Successfully added workout!')
+            return redirect(reverse('workout', args=[workout.id]))
+        else:
+            messages.error(request, 'Failed to add workout. Please ensure the form is valid.')
+    else:
+        form = WorkoutsForm()
+        
+    template = 'workouts/add_workout.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_workout(request, workout_id):
+    """ Edit a workout """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site owners can do that.')
+        return redirect(reverse('workouts'))
+
+    workout = get_object_or_404(Workouts, pk=workout_id)
+    if request.method == 'POST':
+        form = WorkoutsForm(request.POST, request.FILES, instance=workout)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated workout!')
+            return redirect(reverse('workout', args=[workout.id]))
+        else:
+            messages.error(request, 'Failed to update workout. Please ensure the form is valid.')
+    else:
+        form = WorkoutsForm(instance=workout)
+        messages.info(request, f'You are editing {workout.name}')
+
+    template = 'workouts/edit_workout.html'
+    context = {
+        'form': form,
+        'workout': workout,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_workout(request, workout_id):
+    """ Delete a workout  """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only site owners can do that.')
+        return redirect(reverse('workouts'))
+        
+    workout = get_object_or_404(Workouts, pk=workout_id)
+    workout.delete()
+    messages.success(request, 'Workout deleted!')
+    return redirect(reverse('workouts'))
 
 
 
